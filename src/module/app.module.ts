16eedcd83391052ea5829fe367f6c43b903ed7ba
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-08-13 16:12:22 -0300

    feat: use validation pipe

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from '~/module/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
