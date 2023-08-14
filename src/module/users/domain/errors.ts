5d1fbd1d304a2925dc697d4cffca131f73ef5cc7
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-08-13 21:01:45 -0300

    feat: add Age value object

--- src/module/users/domain/errors.ts
+++ src/module/users/domain/errors.ts
@@ -1,8 +1,10 @@
+import { MAX_AGE, MIN_AGE } from '~/module/users/domain/value-objects/age';
 import { MIN_USERNAME_LENGTH } from '~/module/users/domain/value-objects/username';
 
 export enum UserDomainErrors {
   invalidSSOId = 'Invalid SSO ID',
   invalidUsername = `Invalid Username, must be at least ${MIN_USERNAME_LENGTH} characters`,
+  invalidAge = `Invalid Age, must be between ${MIN_AGE} and ${MAX_AGE}`,
 }
 
 export class UserDomainError extends Error {
---
+++ src/module/users/domain/value-objects/age.ts
@@ -0,0 +1,40 @@
+import {
+  UserDomainError,
+  UserDomainErrors,
+  userDomainError,
+} from '~/module/users/domain/errors';
+import { ValueObject } from '~/shared/domain/value-object';
+import { Either, left, right } from '~/shared/either';
+
+export const MIN_AGE = 12;
+export const MAX_AGE = 100;
+
+export type AgeProps = {
+  value: number;
+};
+
+export default class Age extends ValueObject<AgeProps> {
+  private constructor(props: AgeProps) {
+    super(props);
+  }
+
+  get value(): number {
+    return this.props.value;
+  }
+
+  private static isValid(age: number): boolean {
+    return age > MIN_AGE && age < MAX_AGE;
+  }
+
+  public static async create(
+    props: AgeProps,
+  ): Promise<Either<UserDomainError, Age>> {
+    const isValid = this.isValid(props.value);
+    if (!isValid) {
+      return left(userDomainError(UserDomainErrors.invalidAge));
+    }
+
+    const age = new Age(props);
+    return right(age);
+  }
+}
