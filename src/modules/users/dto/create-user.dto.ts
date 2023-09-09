0adb63ed6e65d273192762f4c67418eb9cd4378c
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-09-09 13:58:20 -0300

    feat: add create-user dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { MIN_AGE } from '~/modules/users/domain/value-objects/age';
+import { MIN_HEIGHT } from '~/modules/users/domain/value-objects/height';
+import { MIN_USERNAME_LENGTH } from '~/modules/users/domain/value-objects/username';
+import { MIN_WEIGHT } from '~/modules/users/domain/value-objects/weight';
 
 @Injectable()
 export class CreateUserDto {
   @IsUUID()
   @IsNotEmpty()
   ssoId: string;
+
+  @IsString()
+  @MinLength(MIN_USERNAME_LENGTH)
+  @IsNotEmpty()
+  username: string;
+
+  @IsNumber()
+  @Min(MIN_AGE)
+  @IsNotEmpty()
+  age: number;
+
+  @IsNumber()
+  @Min(MIN_WEIGHT)
+  @IsNotEmpty()
+  weight: number;
+
+  @IsNumber()
+  @Min(MIN_HEIGHT)
+  @IsNotEmpty()
+  height: number;
 }
