12afb245b4d18f172d64950fe7736d7332c45400
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-09-10 13:03:16 -0300

    feat: add userRepository and userMapper to usermodule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import { UsersController } from '~/modules/users/users.controller';
 import { UsersService } from '~/modules/users/users.service';
+import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserRepository, UserMapper],
 })
 export class UsersModule {}
