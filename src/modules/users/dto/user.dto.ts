4008f3b817f6f39068bd9177e8f1f5974902eae7
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-10-09 23:18:52 -0300

    feat: return error if user dto has no id

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,5 +1,8 @@
-import { IsNumber, IsString, IsUUID } from 'class-validator';
+import { HttpStatus } from '@nestjs/common';
+import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 import { UserDomain } from '~/modules/users/domain/users.domain';
+import { UserDtoError } from '~/modules/users/dto/errors/user-dto-errors';
+import { Either, left, right } from '~/shared/either';
 
 export class UserDto {
   @IsUUID()
@@ -12,25 +15,37 @@ export class UserDto {
   username: string;
 
   @IsNumber()
-  age: number;
+  @IsOptional()
+  age?: number;
 
   @IsNumber()
-  weight: number;
+  @IsOptional()
+  weight?: number;
 
   @IsNumber()
-  height: number;
+  @IsOptional()
+  height?: number;
 
-  public static domainToDto(domain: UserDomain): UserDto {
+  public static domainToDto(domain: UserDomain): Either<UserDtoError, UserDto> {
     const { id, ssoId, username, age, weight, height } = domain;
 
+    if (!id) {
+      return left(
+        UserDtoError.create(
+          UserDtoError.messages.missingId,
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    }
+
     const userDto = new UserDto();
-    userDto.id = id?.toString() || null;
+    userDto.id = id.toString();
     userDto.ssoId = ssoId.value;
     userDto.username = username.value;
-    userDto.age = age.value;
-    userDto.weight = weight.value;
-    userDto.height = height.value;
+    userDto.age = age?.value;
+    userDto.weight = weight?.value;
+    userDto.height = height?.value;
 
-    return userDto;
+    return right(userDto);
   }
 }
