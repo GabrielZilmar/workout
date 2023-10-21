cb6002a4a11ae924ecae558dc6d1891a008c45fe
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-10-21 17:08:25 -0300

    feat: is-email-verified vo

---
+++ src/modules/users/domain/value-objects/is-email-verified/index.ts
@@ -0,0 +1,29 @@
+import { ValueObject } from '~/shared/domain/value-object';
+
+export type IsEmailVerifiedProps = {
+  value: boolean;
+};
+
+export default class IsEmailVerified extends ValueObject<IsEmailVerifiedProps> {
+  private constructor(props: IsEmailVerifiedProps) {
+    super(props);
+  }
+
+  get value(): boolean {
+    return this.props.value;
+  }
+
+  public verifyEmail(): this {
+    this.props.value = true;
+    return this;
+  }
+
+  public unVerifyEmail(): this {
+    this.props.value = false;
+    return this;
+  }
+
+  public static create(value = false): IsEmailVerified {
+    return new IsEmailVerified({ value });
+  }
+}
