3e1a23def89a8aa8b09f0d726107b5d9b01f400a
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-10-30 22:42:19 -0300

    feat: add email to user dto

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,5 +1,11 @@
 import { HttpStatus } from '@nestjs/common';
-import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  IsEmail,
+} from 'class-validator';
 import { UserDomain } from '~/modules/users/domain/users.domain';
 import { UserDtoError } from '~/modules/users/dto/errors/user-dto-errors';
 import { Either, left, right } from '~/shared/either';
@@ -10,6 +16,10 @@ export class UserDto {
 
   @IsString()
   username: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
 
   @IsNumber()
   @IsOptional()
@@ -24,7 +34,7 @@ export class UserDto {
   height?: number;
 
   public static domainToDto(domain: UserDomain): Either<UserDtoError, UserDto> {
-    const { id, username, age, weight, height } = domain;
+    const { id, username, email, age, weight, height } = domain;
 
     if (!id) {
       return left(
@@ -38,6 +48,7 @@ export class UserDto {
     const userDto = new UserDto();
     userDto.id = id.toString();
     userDto.username = username.value;
+    userDto.email = email.value;
     userDto.age = age?.value;
     userDto.weight = weight?.value;
     userDto.height = height?.value;
