1d65329e49efc56d2eac88ee6418f02c545e592f
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-09 21:41:39 -0300

    fix: list-users spec

--- src/modules/users/domain/use-cases/list-users/test/list-users.spec.ts
+++ src/modules/users/domain/use-cases/list-users/test/list-users.spec.ts
@@ -1,32 +1,14 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { HttpException, InternalServerErrorException } from '@nestjs/common';
 import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import { ListUsers } from '~/modules/users/domain/use-cases/list-users';
 import { ListUsersMock } from '~/modules/users/domain/use-cases/list-users/test/list-users.mock';
-import { UserDomain } from '~/modules/users/domain/users.domain';
 import { UserDto } from '~/modules/users/dto/user.dto';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 
 jest.mock('~/services/database/typeorm/repositories/users-repository');
 
 describe('List users use case', () => {
-  let useMapper = new UserMapper();
-  let userRepository: UserRepository;
-  let listUsers: ListUsers;
-  let usersDomain: UserDomain[];
-
-  beforeAll(async () => {
-    useMapper = new UserMapper();
-
-    usersDomain = await ListUsersMock.mountUsersDomains();
-    const repositoryListUserMock = jest.fn().mockResolvedValue(usersDomain);
-    const userRepositoryMock = new UserRepository(useMapper) as jest.Mocked<
-      InstanceType<typeof UserRepository>
-    >;
-    userRepositoryMock.find = repositoryListUserMock;
-
-    userRepository = userRepositoryMock;
-    listUsers = new ListUsers(userRepository);
-  });
+  const useMapper = new UserMapper();
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -34,35 +16,51 @@ describe('List users use case', () => {
   });
 
   it('Should list all users', async () => {
-    const usersDto = await Promise.all(
-      usersDomain.map((user) => UserDto.domainToDto(user).value),
-    );
-
-    const users = await listUsers.execute({});
-    expect(users).toMatchObject(usersDto);
-  });
-
-  it('Should throw http exception if failed to pass users to dto', async () => {
-    const invalidUsersDomain = ListUsersMock.mountUsersDomains(true);
+    const usersDomain = await ListUsersMock.mountUsersDomains();
     const userRepositoryMock = new UserRepository(useMapper) as jest.Mocked<
       InstanceType<typeof UserRepository>
     >;
     const repositoryListUserMock = jest
       .fn()
-      .mockRejectedValue(invalidUsersDomain);
+      .mockResolvedValue({ items: usersDomain, count: usersDomain.length });
     userRepositoryMock.find = repositoryListUserMock;
-    userRepository = userRepositoryMock;
+    const userRepository = userRepositoryMock;
+    const listUsers = new ListUsers(userRepository);
+
+    const usersDto = await Promise.all(
+      usersDomain.map((user) => UserDto.domainToDto(user).value),
+    );
+
+    const users = await listUsers.execute({});
+    expect(users).toMatchObject({ users: usersDto, count: usersDto.length });
+  });
+
+  it('Should throw http exception if failed to pass users to dto', async () => {
+    const invalidUsersDomain = await ListUsersMock.mountUsersDomains(true);
+    const userRepositoryMock = new UserRepository(useMapper) as jest.Mocked<
+      InstanceType<typeof UserRepository>
+    >;
+    const repositoryListUserMock = jest.fn().mockResolvedValue({
+      items: invalidUsersDomain,
+      count: invalidUsersDomain.length,
+    });
+    userRepositoryMock.find = repositoryListUserMock;
+    const userRepository = userRepositoryMock;
+    const listUsers = new ListUsers(userRepository);
+
+    await expect(listUsers.execute({})).rejects.toThrowError(HttpException);
   });
 
   it('Should throw internal error if user repository failed to get users', async () => {
     const userRepositoryMock = new UserRepository(useMapper) as jest.Mocked<
       InstanceType<typeof UserRepository>
     >;
-    const repositoryListUserMock = jest.fn().mockRejectedValue(usersDomain);
+    const repositoryListUserMock = jest.fn().mockRejectedValue(false);
     userRepositoryMock.find = repositoryListUserMock;
-    userRepository = userRepositoryMock;
+    const userRepository = userRepositoryMock;
+    const listUsers = new ListUsers(userRepository);
 
-    await expect(listUsers.execute({})).rejects.toThrow(
+    await expect(listUsers.execute({})).rejects.toThrowError(
       InternalServerErrorException,
     );
   });
