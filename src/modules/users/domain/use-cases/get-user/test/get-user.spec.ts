7f015774ad4de93d5cdfe4fc4a00ac8a800536c7
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-09 22:01:11 -0300

    feat: add get user by id test

---
+++ src/modules/users/domain/use-cases/get-user/test/get-user.spec.ts
@@ -0,0 +1,32 @@
+import { HttpException } from '@nestjs/common';
+import UserMapper from '~/modules/users/domain/mappers/users.mapper';
+import { GetUser } from '~/modules/users/domain/use-cases/get-user';
+import { GetUserMock } from '~/modules/users/domain/use-cases/get-user/test/get-user.mock';
+import { UserDto } from '~/modules/users/dto/user.dto';
+import UserRepository from '~/services/database/typeorm/repositories/users-repository';
+
+describe('Get user use case', () => {
+  const useMapper = new UserMapper();
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it('Should get user by id or username', async () => {
+    const userDomain = await GetUserMock.mountUserDomain();
+    const userRepositoryMock = new UserRepository(useMapper) as jest.Mocked<
+      InstanceType<typeof UserRepository>
+    >;
+    const repositoryGetUserMock = jest.fn().mockResolvedValue(userDomain);
+    userRepositoryMock.findOne = repositoryGetUserMock;
+    const userRepository = userRepositoryMock;
+    const getUser = new GetUser(userRepository);
+
+    const userDto = UserDto.domainToDto(userDomain).value;
+    const user = await getUser.execute({
+      idOrUsername: GetUserMock.userMock.id,
+    });
+    expect(user).toMatchObject(userDto);
+  });
+});
