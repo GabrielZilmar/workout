e17503b223856f3481f4999d5188af971de0dfec
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-11 14:20:49 -0300

    feat: get users before update

--- src/modules/users/domain/use-cases/update-user/index.ts
+++ src/modules/users/domain/use-cases/update-user/index.ts
@@ -1,4 +1,11 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import UserMapper from '~/modules/users/domain/mappers/users.mapper';
+import { UserUseCaseError } from '~/modules/users/domain/use-cases/errors';
 import {
   CreateUserParamsDto,
   UpdateUserBodyDto,
@@ -6,30 +13,48 @@ import {
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 import { UseCase } from '~/shared/core/use-case';
 
-type UpdateUserParams = CreateUserParamsDto & UpdateUserBodyDto;
-type UpdateUserResult = Promise<boolean>;
+export type UpdateUserParams = CreateUserParamsDto & UpdateUserBodyDto;
+export type UpdateUserResult = Promise<boolean>;
 
 @Injectable()
 export class UpdateUser implements UseCase<UpdateUserParams, UpdateUserResult> {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly userMapper: UserMapper,
+  ) {}
 
   public async execute({ id, ...params }: UpdateUserParams): UpdateUserResult {
-    try {
-      const userUpdatedOrError = await this.userRepository.update(id, params);
+    const user = await this.userRepository.findOneById(id);
+    if (!user) {
+      throw new HttpException(
+        UserUseCaseError.messages.userNotFound(id),
+        HttpStatus.NOT_FOUND,
+      );
+    }
 
-      if (userUpdatedOrError.isLeft()) {
-        throw new HttpException(
-          {
-            message: userUpdatedOrError.value.message,
-            duplicatedItems: userUpdatedOrError.value.payload,
-          },
-          userUpdatedOrError.value.code,
-        );
+    // TODO: Create update user method in UserDomain
+    const userData = this.userMapper.toPersistence(user);
+
+    const userUpdatedOrError = await this.userRepository.update(id, {
+      ...userData,
+      ...params,
+    });
+
+    if (userUpdatedOrError.isLeft()) {
+      if (userUpdatedOrError.value.code === HttpStatus.BAD_REQUEST) {
+        throw new BadRequestException({
+          statusCode: userUpdatedOrError.value.code,
+          message: userUpdatedOrError.value.message,
+          duplicatedItems: userUpdatedOrError.value.payload,
+        });
       }
 
-      return userUpdatedOrError.value;
-    } catch (e) {
-      throw new Error((e as Error).message);
+      throw new HttpException(
+        userUpdatedOrError.value.message,
+        userUpdatedOrError.value.code,
+      );
     }
+
+    return userUpdatedOrError.value;
   }
 }
