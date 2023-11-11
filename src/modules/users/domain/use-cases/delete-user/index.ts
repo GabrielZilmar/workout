c5af423634f4887a4e69311795b25c5f8c6957c4
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-11 16:38:17 -0300

    feat: add delete user use case

---
+++ src/modules/users/domain/use-cases/delete-user/index.ts
@@ -0,0 +1,39 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { UserUseCaseError } from '~/modules/users/domain/use-cases/errors';
+import { DeleteUserParamsDto } from '~/modules/users/dto/delete-user.dto';
+import UserRepository from '~/services/database/typeorm/repositories/users-repository';
+import { UseCase } from '~/shared/core/use-case';
+
+type DeleteUserParams = DeleteUserParamsDto;
+type DeleteUserResult = Promise<boolean>;
+
+@Injectable()
+export class DeleteUser implements UseCase<DeleteUserParams, DeleteUserResult> {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  public async execute({ id }: DeleteUserParams): DeleteUserResult {
+    const user = await this.userRepository.findOneById(id);
+    if (!user) {
+      throw new HttpException(
+        UserUseCaseError.messages.userNotFound(id),
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    user.delete();
+
+    try {
+      const userHasBeenDeleted =
+        await this.userRepository.repository.softDelete(id);
+
+      return !!userHasBeenDeleted.affected;
+    } catch (error) {
+      throw new InternalServerErrorException((error as Error).message);
+    }
+  }
+}
