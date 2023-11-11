b5fb630fba719e442745c27b6649f99ff5d890ea
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-11 17:15:31 -0300

    feat: user doesnt exist delete user test

--- src/modules/users/domain/use-cases/delete-user/delete-user.spec.ts
+++ src/modules/users/domain/use-cases/delete-user/delete-user.spec.ts
@@ -1,6 +1,8 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import { DeleteUser } from '~/modules/users/domain/use-cases/delete-user';
+import { UserUseCaseError } from '~/modules/users/domain/use-cases/errors';
 import { GetUserMock } from '~/modules/users/domain/use-cases/get-user/test/get-user.mock';
 import { UserDomain } from '~/modules/users/domain/users.domain';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
@@ -44,5 +46,18 @@ describe('Delete user use case', () => {
   it('Should delete a user', async () => {
     const userHasBeenUpdated = await deleteUser.execute({ id: uuid() });
     expect(userHasBeenUpdated).toBeTruthy();
+  });
+
+  it('Should not delete a user if it user does not exists', async () => {
+    const repositoryFindOneByIdMock = jest.fn().mockResolvedValue(null);
+    userRepository.findOneById = repositoryFindOneByIdMock;
+
+    const id = uuid();
+    await expect(deleteUser.execute({ id })).rejects.toThrowError(
+      new HttpException(
+        UserUseCaseError.messages.userNotFound(id),
+        HttpStatus.NOT_FOUND,
+      ),
+    );
   });
 });
