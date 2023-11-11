942ab9d35ee1267bae049080bc9fac65bd3dcb55
GabrielZilmarSP <gabriel.caetano@shawandpartners.com>
2023-11-10 23:11:15 -0300

    feat: call update user in the user controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,18 +3,22 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
+  Put,
 } from '@nestjs/common';
 import { CreateUser } from '~/modules/users/domain/use-cases/create-user';
 import { GetUser } from '~/modules/users/domain/use-cases/get-user';
 import { ListUsers } from '~/modules/users/domain/use-cases/list-users';
+import { UpdateUser } from '~/modules/users/domain/use-cases/update-user';
 import { CreateUserDto } from '~/modules/users/dto/create-user.dto';
 import { FindAllUsersDto } from '~/modules/users/dto/find-all-users.dto';
 import { GetUserDto } from '~/modules/users/dto/get-user.dto';
-import { UpdateUserDto } from '~/modules/users/dto/update-user.dto';
+import {
+  CreateUserParamsDto,
+  UpdateUserBodyDto,
+} from '~/modules/users/dto/update-user.dto';
 
 @Controller('/api/users')
 export class UsersController {
@@ -22,6 +26,7 @@ export class UsersController {
     private readonly createUser: CreateUser,
     private readonly listUsers: ListUsers,
     private readonly getUser: GetUser,
+    private readonly updateUser: UpdateUser,
   ) {}
 
   @Post()
@@ -39,9 +44,15 @@ export class UsersController {
     return this.getUser.execute(idOrUsername);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  @Put(':id')
+  update(
+    @Param() id: CreateUserParamsDto,
+    @Body() updateUserDto: UpdateUserBodyDto,
+  ) {
+    return this.updateUser.execute({
+      ...id,
+      ...updateUserDto,
+    });
   }
 
   @Delete(':id')
