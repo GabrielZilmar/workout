84a273fd79ef80ea3601a786f4eee0131d68d137
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-12-19 23:27:59 -0300

    feat: seesion module providers

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import SessionUseCaseProviders from '~/modules/session/domain/use-cases/providers';
 import { SessionController } from '~/modules/session/session.controller';
+import UserMapper from '~/modules/users/domain/mappers/users.mapper';
+import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 
 @Module({
+  imports: [],
   controllers: [SessionController],
-  providers: [],
+  providers: [UserRepository, UserMapper, ...SessionUseCaseProviders],
 })
 export class SessionModule {}
