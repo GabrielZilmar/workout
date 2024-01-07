15cbcea35687d47a2d79b2bc6ec072d47f07796f
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-01-07 18:52:46 -0300

    feat: add mailerModule to session module

--- src/modules/session/session.controller.spec.ts
+++ src/modules/session/session.controller.spec.ts
@@ -1,16 +1,36 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Test, TestingModule } from '@nestjs/testing';
 import SessionUseCaseProviders from '~/modules/session/domain/use-cases/providers';
 import { SessionController } from '~/modules/session/session.controller';
 import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
+import EmailSender from '~/services/email-sender';
 
 describe('SessionController', () => {
   let controller: SessionController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MailerModule.forRoot({
+          transport: {
+            host: 'emailHost',
+            port: 0,
+            secure: true,
+            auth: {
+              user: 'emailSender',
+              pass: 'emailPassword',
+            },
+          },
+        }),
+      ],
       controllers: [SessionController],
-      providers: [UserRepository, UserMapper, ...SessionUseCaseProviders],
+      providers: [
+        UserRepository,
+        UserMapper,
+        EmailSender,
+        ...SessionUseCaseProviders,
+      ],
     }).compile();
 
     controller = module.get<SessionController>(SessionController);
--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,12 +1,32 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import SessionUseCaseProviders from '~/modules/session/domain/use-cases/providers';
 import { SessionController } from '~/modules/session/session.controller';
 import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
+import EmailSender from '~/services/email-sender';
+import Env from '~/shared/env';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: Env.emailHost,
+        port: Env.emailPort,
+        secure: true,
+        auth: {
+          user: Env.emailSender,
+          pass: Env.emailPassword,
+        },
+      },
+    }),
+  ],
   controllers: [SessionController],
-  providers: [UserRepository, UserMapper, ...SessionUseCaseProviders],
+  providers: [
+    UserRepository,
+    UserMapper,
+    EmailSender,
+    ...SessionUseCaseProviders,
+  ],
 })
 export class SessionModule {}
