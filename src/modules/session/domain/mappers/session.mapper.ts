21595b7fd24b0bcf40b495e71ae15245ab10a0c6
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-02-21 21:20:32 -0300

    feat: add session mapper

---
+++ src/modules/session/domain/mappers/session.mapper.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import SessionDomain from '~/modules/session/domain/session.domain';
+import { Token as TokenEntity } from '~/modules/session/entities/token.entity';
+import { Mapper } from '~/shared/domain/mapper';
+import { UniqueEntityID } from '~/shared/domain/unique-entity-id';
+import { Either, left, right } from '~/shared/either';
+
+@Injectable()
+export default class SessionMapper
+  implements Mapper<SessionDomain, Partial<TokenEntity>>
+{
+  public async toDomain(
+    raw: TokenEntity,
+  ): Promise<Either<Error, SessionDomain>> {
+    const { id, userId, token, type } = raw;
+
+    const entityId = new UniqueEntityID(id);
+    const sessionOrError = SessionDomain.create(
+      {
+        userId,
+        token: {
+          value: token,
+          isEncrypted: true,
+        },
+        tokenType: type,
+      },
+      entityId,
+    );
+
+    if (sessionOrError.isLeft()) {
+      return left(sessionOrError.value);
+    }
+
+    return right(sessionOrError.value);
+  }
+
+  public toPersistence(item: SessionDomain): Partial<TokenEntity> {
+    const { id, userId, token, tokenType } = item;
+
+    const tokenEntity: Partial<TokenEntity> = {
+      id: id?.toString(),
+      userId,
+      token: token.value,
+      expiry: token.props.expiry,
+      type: tokenType.value,
+    };
+
+    return tokenEntity;
+  }
+}
