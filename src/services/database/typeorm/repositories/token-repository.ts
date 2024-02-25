b94deba2237e05d6a36fae002ef3fe4356e8a054
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-02-24 21:15:33 -0300

    feat: add findLastByUserIdAndType method to TokenRepository

--- src/services/database/typeorm/repositories/token-repository.ts
+++ src/services/database/typeorm/repositories/token-repository.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { IsNull, MoreThanOrEqual } from 'typeorm';
 import SessionMapper from '~/modules/session/domain/mappers/session.mapper';
 import SessionDomain from '~/modules/session/domain/session.domain';
-import { Token } from '~/modules/session/entities/token.entity';
+import { Token, TokenTypes } from '~/modules/session/entities/token.entity';
 import { BaseRepository } from '~/services/database/typeorm/repositories/base/base-repository';
 
 @Injectable()
@@ -11,5 +12,18 @@ export default class TokenRepository extends BaseRepository<
 > {
   constructor(sessionMapper: SessionMapper) {
     super(Token, sessionMapper);
+  }
+
+  async findLastByUserIdAndType(userId: string, type: TokenTypes) {
+    const token = await this.findOne({
+      where: {
+        userId,
+        type,
+        usedAt: IsNull(),
+        expiry: MoreThanOrEqual(new Date()),
+      },
+    });
+
+    return token;
   }
 }
