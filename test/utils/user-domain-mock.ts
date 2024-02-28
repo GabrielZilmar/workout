e04f38fb515232aadcc3929d553a3d07dfdd2d10
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-02-28 19:55:51 -0300

    feat: improve user-domain-mock with optional props attributes

--- test/utils/user-domain-mock.ts
+++ test/utils/user-domain-mock.ts
@@ -5,6 +5,11 @@ import {
 import { User } from '~/modules/users/entities/user.entity';
 import { UniqueEntityID } from '~/shared/domain/unique-entity-id';
 
+type MountUserDomainParams = Partial<UserDomainCreateParams> & {
+  id?: string;
+  withoutId?: boolean;
+};
+
 export class UserDomainMock {
   public static userMockParams: User = {
     id: '46ccf0f8-ec5c-46f0-ae4e-cff06a4b01fe',
@@ -21,28 +26,31 @@ export class UserDomainMock {
     updatedAt: new Date(),
   };
 
-  public static async mountUserDomain(withoutId = false): Promise<UserDomain> {
+  public static async mountUserDomain({
+    withoutId,
+    ...props
+  }: MountUserDomainParams = {}): Promise<UserDomain> {
     const user = this.userMockParams;
 
     const userParams = {
       params: {
-        username: user.username,
-        email: user.email,
+        username: props.username || user.username,
+        email: props.email || user.email,
         password: {
-          value: user.password,
+          value: props.password || user.password,
         },
-        age: user.age,
-        weight: user.weight,
-        height: user.height,
-        isEmailVerified: user.isEmailVerified,
-        isAdmin: user.isAdmin,
-        deletedAt: user.deletedAt,
+        age: props.age || user.age,
+        weight: props.weight || user.weight,
+        height: props.height || user.height,
+        isEmailVerified: props.isEmailVerified || user.isEmailVerified,
+        isAdmin: props.isAdmin || user.isAdmin,
+        deletedAt: props.deletedAt || user.deletedAt,
       } as UserDomainCreateParams,
-      id: user.id,
+      id: props.id || user.id,
     };
     let id: UniqueEntityID | undefined;
     if (!withoutId) {
-      id = new UniqueEntityID(user.id);
+      id = new UniqueEntityID(props.id || user.id);
     }
 
     const userDomain = await UserDomain.create(userParams.params, id);
