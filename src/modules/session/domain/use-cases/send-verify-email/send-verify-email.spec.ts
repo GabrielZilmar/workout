d116a21ffdad1cd3da81a44bfe8a75eb0ddda290
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-02-28 16:46:14 -0300

    refactor: verify correct exception

--- src/modules/session/domain/use-cases/send-verify-email/send-verify-email.spec.ts
+++ src/modules/session/domain/use-cases/send-verify-email/send-verify-email.spec.ts
@@ -1,29 +1,55 @@
 import { MailerModule, MailerService } from '@nestjs-modules/mailer';
-import { Provider } from '@nestjs/common';
+import { HttpException, HttpStatus, Provider } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserDomainMock } from 'test/utils/user-domain-mock';
+import SessionMapper from '~/modules/session/domain/mappers/session.mapper';
+import { SessionUseCaseError } from '~/modules/session/domain/use-cases/errors';
 import { SendVerifyEmail } from '~/modules/session/domain/use-cases/send-verify-email';
 import UserMapper from '~/modules/users/domain/mappers/users.mapper';
 import { UserDomain } from '~/modules/users/domain/users.domain';
+import TokenRepository from '~/services/database/typeorm/repositories/token-repository';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 import EmailSender from '~/services/email-sender';
 
 describe('SendVerifyEmail Use Case', () => {
+  let tokenRepository: TokenRepository;
   let userRepository: UserRepository;
   let userDomain: UserDomain;
   let sendVerifyEmail: SendVerifyEmail;
   let module: TestingModule;
+
+  const getTokenRepositoryProvider = (tokenRepositoryMock?: TokenRepository) =>
+    ({
+      provide: TokenRepository,
+      useFactory: (sessionMapper: SessionMapper) => {
+        if (!tokenRepositoryMock) {
+          const findLastByUserIdAndTypedMock = jest
+            .fn()
+            .mockResolvedValue(null);
+          tokenRepositoryMock = new TokenRepository(
+            sessionMapper,
+          ) as jest.Mocked<InstanceType<typeof TokenRepository>>;
+          tokenRepositoryMock.findLastByUserIdAndType =
+            findLastByUserIdAndTypedMock;
+        }
+
+        tokenRepository = tokenRepositoryMock;
+
+        return tokenRepository;
+      },
+      inject: [SessionMapper],
+    } as Provider);
 
   const getUserRepositoryProvider = (userRepositoryMock?: UserRepository) =>
     ({
       provide: UserRepository,
       useFactory: (userMapper: UserMapper) => {
         if (!userRepositoryMock) {
-          const findByEmailIdMock = jest.fn().mockResolvedValue(userDomain);
+          const findOneByIdMock = jest.fn().mockResolvedValue(userDomain);
           userRepositoryMock = new UserRepository(userMapper) as jest.Mocked<
             InstanceType<typeof UserRepository>
           >;
-          userRepositoryMock.findOneById = findByEmailIdMock;
+          userRepositoryMock.findOneById = findOneByIdMock;
         }
 
         userRepository = userRepositoryMock;
@@ -47,6 +73,7 @@ describe('SendVerifyEmail Use Case', () => {
 
   const getModuleTest = async (
     userRepositoryProvider = getUserRepositoryProvider(),
+    tokenRepositoryProvider = getTokenRepositoryProvider(),
   ) => {
     const emailSender = getEmailSenderProvider();
 
@@ -66,8 +93,10 @@ describe('SendVerifyEmail Use Case', () => {
       ],
       providers: [
         userRepositoryProvider,
-        UserMapper,
+        tokenRepositoryProvider,
         emailSender,
+        UserMapper,
+        SessionMapper,
         SendVerifyEmail,
       ],
     }).compile();
@@ -93,47 +122,33 @@ describe('SendVerifyEmail Use Case', () => {
     expect(sendMailSpy).toHaveBeenCalled();
   });
 
-  it('Should not send verify email if user does not exists and repository throw an error', async () => {
-    const userMapper = new UserMapper();
-    const findByEmailIdMock = jest.fn().mockRejectedValue(new Error());
-    const userRepositoryMock = new UserRepository(userMapper) as jest.Mocked<
-      InstanceType<typeof UserRepository>
-    >;
-    userRepositoryMock.findOneById = findByEmailIdMock;
-
-    const userRepositoryProvider =
-      getUserRepositoryProvider(userRepositoryMock);
-
-    const module = await getModuleTest(userRepositoryProvider);
-    const sendVerifyEmail = module.get<SendVerifyEmail>(SendVerifyEmail);
-
-    await expect(
-      sendVerifyEmail.execute({
-        userId: 'non-existing-id',
-        baseUrl: 'http://localhost:3000',
-      }),
-    ).rejects.toThrowError();
-  });
-
   it('Should not send verify email if not find user and repository return null', async () => {
     const userMapper = new UserMapper();
-    const findByEmailIdMock = jest.fn().mockResolvedValue(() => null);
+    const findOneByIdMock = jest.fn().mockResolvedValue(() => null);
     const userRepositoryMock = new UserRepository(userMapper) as jest.Mocked<
       InstanceType<typeof UserRepository>
     >;
-    userRepositoryMock.findOneById = findByEmailIdMock;
+    userRepositoryMock.findOneById = findOneByIdMock;
 
     const userRepositoryProvider =
       getUserRepositoryProvider(userRepositoryMock);
     const module = await getModuleTest(userRepositoryProvider);
     const sendVerifyEmail = module.get<SendVerifyEmail>(SendVerifyEmail);
 
+    const userId = 'non-existing-id';
     await expect(
       sendVerifyEmail.execute({
-        userId: 'non-existing-id',
+        userId,
         baseUrl: 'http://localhost:3000',
       }),
-    ).rejects.toThrowError();
+    ).rejects.toThrow(
+      new HttpException(
+        {
+          message: SessionUseCaseError.messages.userIdNotFound(userId),
+        },
+        HttpStatus.NOT_FOUND,
+      ),
+    );
   });
 
   // it('Should not send verify email if user is already verified', () => {});
