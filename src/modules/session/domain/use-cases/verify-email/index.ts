b027c8859ba56a6a0ba04c60d1ee82fc42753546
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-01 12:22:58 -0300

    feat: add verify email use case

---
+++ src/modules/session/domain/use-cases/verify-email/index.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { VerifyEmailDto } from '~/modules/session/dto/verify-email.dto';
+import { UseCase } from '~/shared/core/use-case';
+
+export type VerifyEmailParams = VerifyEmailDto;
+export type VerifyEmailResult = Promise<boolean>;
+
+@Injectable()
+export class VerifyEmail
+  implements UseCase<VerifyEmailParams, VerifyEmailResult>
+{
+  execute({ verifyEmailToken }: VerifyEmailParams): VerifyEmailResult {
+    throw new Error('Method not implemented.');
+  }
+}
