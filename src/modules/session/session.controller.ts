2d05c6dce7451e17ea3cd43e00f1b1e1ede4b1be
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-01 12:23:39 -0300

    feat: verify-email to session controller

--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Post, Body, Req } from '@nestjs/common';
+import { Controller, Post, Body, Req, Query, Get } from '@nestjs/common';
 import { Request } from 'express';
 import { Login } from '~/modules/session/domain/use-cases/login';
 import { SendVerifyEmail } from '~/modules/session/domain/use-cases/send-verify-email';
+import { VerifyEmail } from '~/modules/session/domain/use-cases/verify-email';
 import { SessionLoginDto } from '~/modules/session/dto/login.dto';
 import { SendVerifyEmailDto } from '~/modules/session/dto/send-verify-email.dto';
+import { VerifyEmailDto } from '~/modules/session/dto/verify-email.dto';
 
 @Controller('/api/session')
 export class SessionController {
   constructor(
     private readonly loginUseCase: Login,
     private readonly sendVerifyEmailUseCase: SendVerifyEmail,
+    private readonly verifyEmailUseCase: VerifyEmail,
   ) {}
 
   @Post('/login')
@@ -27,5 +30,10 @@ export class SessionController {
       ...sendVerifyEmailDto,
       baseUrl,
     });
+  }
+
+  @Get('/verify-email')
+  verifyEmail(@Req() req: Request, @Query() verifyEmailDto: VerifyEmailDto) {
+    return this.verifyEmailUseCase.execute(verifyEmailDto);
   }
 }
