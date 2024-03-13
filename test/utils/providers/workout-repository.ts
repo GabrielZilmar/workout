368fe880b358cb934843a923d3c9eac6c287f403
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-13 08:40:23 -0300

    feat: add workout repository

---
+++ test/utils/providers/workout-repository.ts
@@ -0,0 +1,38 @@
+import { Provider } from '@nestjs/common';
+import { WorkoutDomainMock } from 'test/utils/domains/workout-domain-mock';
+import WorkoutDomain from '~/modules/workout/domain/workout.domain';
+import WorkoutMapper from '~/modules/workout/mappers/workout.mapper';
+import WorkoutRepository from '~/services/database/typeorm/repositories/workout-repository';
+import { right } from '~/shared/either';
+
+type GetWorkoutRepositoryProviderParams = {
+  workoutRepositoryMock?: WorkoutRepository;
+  workoutDomain?: WorkoutDomain;
+};
+
+const getWorkoutRepositoryProvider = ({
+  workoutRepositoryMock,
+  workoutDomain,
+}: GetWorkoutRepositoryProviderParams = {}) => {
+  if (!workoutDomain) {
+    workoutDomain = WorkoutDomainMock.mountWorkoutDomain();
+  }
+
+  return {
+    provide: WorkoutRepository,
+    useFactory: () => {
+      if (!workoutRepositoryMock) {
+        const createMock = jest.fn().mockResolvedValue(right(workoutDomain));
+        workoutRepositoryMock = new WorkoutRepository(
+          new WorkoutMapper(),
+        ) as jest.Mocked<InstanceType<typeof WorkoutRepository>>;
+        workoutRepositoryMock.create = createMock;
+      }
+
+      return workoutRepositoryMock;
+    },
+    inject: [WorkoutMapper],
+  } as Provider;
+};
+
+export default getWorkoutRepositoryProvider;
