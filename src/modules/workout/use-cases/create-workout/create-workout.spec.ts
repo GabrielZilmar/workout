501452a379761a05ab9287cc153ff76be7c4e133
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-13 08:44:53 -0300

    feat: add create-workout spec mocks

---
+++ src/modules/workout/use-cases/create-workout/create-workout.spec.ts
@@ -0,0 +1,74 @@
+import { Provider } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserDomainMock } from 'test/utils/domains/user-domain-mock';
+import { WorkoutDomainMock } from 'test/utils/domains/workout-domain-mock';
+import getUserRepositoryProvider from 'test/utils/providers/user-repository';
+import getWorkoutRepositoryProvider from 'test/utils/providers/workout-repository';
+import UserMapper from '~/modules/users/domain/mappers/users.mapper';
+import { UserDomain } from '~/modules/users/domain/users.domain';
+import WorkoutDomain from '~/modules/workout/domain/workout.domain';
+import WorkoutMapper from '~/modules/workout/mappers/workout.mapper';
+import { CreateWorkout } from '~/modules/workout/use-cases/create-workout';
+import UserRepository from '~/services/database/typeorm/repositories/users-repository';
+
+type GetModuleTestParams = {
+  userRepositoryProvider?: Provider;
+  workoutRepositoryProvider?: Provider;
+};
+
+describe('CreateWorkout use case', () => {
+  let userDomain: UserDomain;
+  let workoutDomain: WorkoutDomain;
+  const userMapper = new UserMapper();
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    userDomain = await UserDomainMock.mountUserDomain();
+    workoutDomain = WorkoutDomainMock.mountWorkoutDomain();
+    module = await getModuleTest();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  const getUserRepositoryMock = () => {
+    const findOneByIdMock = jest.fn().mockResolvedValue(userDomain);
+    const userRepositoryMock = new UserRepository(userMapper) as jest.Mocked<
+      InstanceType<typeof UserRepository>
+    >;
+    userRepositoryMock.findOneById = findOneByIdMock;
+
+    return userRepositoryMock;
+  };
+
+  const getModuleTest = async ({
+    userRepositoryProvider,
+    workoutRepositoryProvider,
+  }: GetModuleTestParams = {}) => {
+    if (!userRepositoryProvider) {
+      userRepositoryProvider = await getUserRepositoryProvider({
+        userRepositoryMock: getUserRepositoryMock(),
+        userDomain,
+      });
+    }
+
+    if (!workoutRepositoryProvider) {
+      workoutRepositoryProvider = getWorkoutRepositoryProvider({
+        workoutDomain,
+      });
+    }
+
+    return Test.createTestingModule({
+      imports: [],
+      providers: [
+        userRepositoryProvider,
+        workoutRepositoryProvider,
+        UserMapper,
+        WorkoutMapper,
+        CreateWorkout,
+      ],
+    }).compile();
+  };
+});
