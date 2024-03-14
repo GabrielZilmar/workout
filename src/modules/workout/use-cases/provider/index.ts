168ac71286036c751ad1f3a025cc7cfcc37cbef4
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-14 16:30:00 -0300

    feat: update workout use case

--- src/modules/workout/use-cases/provider/index.ts
+++ src/modules/workout/use-cases/provider/index.ts
@@ -2,12 +2,14 @@ import { CreateWorkout } from '~/modules/workout/use-cases/create-workout';
 import { GetWorkout } from '~/modules/workout/use-cases/get-workout';
 import { ListPublicWorkouts } from '~/modules/workout/use-cases/list-public-workouts';
 import { ListWorkouts } from '~/modules/workout/use-cases/list-workouts';
+import { UpdateWorkout } from '~/modules/workout/use-cases/update-workout';
 
 const workoutUseCaseProviders = [
   CreateWorkout,
   ListWorkouts,
   ListPublicWorkouts,
   GetWorkout,
+  UpdateWorkout,
 ];
 
 export default workoutUseCaseProviders;
