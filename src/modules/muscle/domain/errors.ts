fd773a992f905b593e97b8ce3bae883e06fc7722
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-15 13:56:36 -0300

    feat: muscle domain error

---
+++ src/modules/muscle/domain/errors.ts
@@ -0,0 +1,22 @@
+import { HttpStatus } from '@nestjs/common';
+import { MIN_MUSCLE_NAME_LENGTH } from '~/modules/muscle/domain/value-objects/name';
+
+export class MuscleDomainError extends Error {
+  public readonly code: number;
+  public static messages = {
+    invalidMuscleName: `Invalid Username, must be at least ${MIN_MUSCLE_NAME_LENGTH} characters`,
+  };
+
+  constructor(message: string, code: number) {
+    super(message);
+    this.name = 'exercise-domain';
+    this.code = code;
+  }
+
+  public static create(
+    message: string,
+    code = HttpStatus.INTERNAL_SERVER_ERROR,
+  ) {
+    return new MuscleDomainError(message, code);
+  }
+}
