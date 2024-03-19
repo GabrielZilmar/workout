f37620841452b69584ba112f06c4b521225de652
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 15:30:42 -0300

    feat: add muscle findAll route

--- src/modules/muscle/muscle.controller.ts
+++ src/modules/muscle/muscle.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateMuscleDto } from '~/modules/muscle/dto/create-muscle.dto';
+import { ListMusclesDto } from '~/modules/muscle/dto/list-muscles.dto';
 import { CreateMuscle } from '~/modules/muscle/use-cases/create-muscle';
+import { ListMuscle } from '~/modules/muscle/use-cases/list-muscle';
 
 @Controller('/api/muscles')
-@UseGuards(AuthGuard, AdminGuard)
+@UseGuards(AuthGuard)
 export class MuscleController {
-  constructor(private readonly createMuscle: CreateMuscle) {}
+  constructor(
+    private readonly createMuscle: CreateMuscle,
+    private readonly listMuscle: ListMuscle,
+  ) {}
 
   @Post()
+  @UseGuards(AdminGuard)
   create(@Body() createMuscleDto: CreateMuscleDto) {
     return this.createMuscle.execute(createMuscleDto);
+  }
+
+  @Get()
+  findAll(@Query() query: ListMusclesDto) {
+    return this.listMuscle.execute({ ...query });
   }
 }
