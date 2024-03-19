fcf2b21b46e03c9b68bd481e4515f95ef98353f5
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 17:07:59 -0300

    feat: update muscle route

--- src/modules/muscle/muscle.controller.ts
+++ src/modules/muscle/muscle.controller.ts
@@ -1,10 +1,24 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateMuscleDto } from '~/modules/muscle/dto/create-muscle.dto';
 import { ListMusclesDto } from '~/modules/muscle/dto/list-muscles.dto';
+import {
+  UpdateMuscleBodyDto,
+  UpdateMuscleParamsDto,
+} from '~/modules/muscle/dto/update-muscle.dto';
 import { CreateMuscle } from '~/modules/muscle/use-cases/create-muscle';
 import { ListMuscle } from '~/modules/muscle/use-cases/list-muscle';
+import { UpdateMuscle } from '~/modules/muscle/use-cases/update-muscle';
 
 @Controller('/api/muscles')
 @UseGuards(AuthGuard)
@@ -12,6 +26,7 @@ export class MuscleController {
   constructor(
     private readonly createMuscle: CreateMuscle,
     private readonly listMuscle: ListMuscle,
+    private readonly updateMuscle: UpdateMuscle,
   ) {}
 
   @Post()
@@ -23,5 +38,14 @@ export class MuscleController {
   @Get()
   findAll(@Query() query: ListMusclesDto) {
     return this.listMuscle.execute({ ...query });
+  }
+
+  @Put(':id')
+  @UseGuards(AdminGuard)
+  update(
+    @Body() body: UpdateMuscleBodyDto,
+    @Param() { id }: UpdateMuscleParamsDto,
+  ) {
+    return this.updateMuscle.execute({ id, ...body });
   }
 }
