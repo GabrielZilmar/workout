10983a2144f827a891a8f064f3d9da885bb00a53
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 17:07:43 -0300

    feat: update muscle use case

--- src/modules/muscle/use-cases/provider/index.ts
+++ src/modules/muscle/use-cases/provider/index.ts
@@ -1,6 +1,7 @@
 import { CreateMuscle } from '~/modules/muscle/use-cases/create-muscle';
 import { ListMuscle } from '~/modules/muscle/use-cases/list-muscle';
+import { UpdateMuscle } from '~/modules/muscle/use-cases/update-muscle';
 
-const useCaseProviders = [CreateMuscle, ListMuscle];
+const useCaseProviders = [CreateMuscle, ListMuscle, UpdateMuscle];
 
 export default useCaseProviders;
