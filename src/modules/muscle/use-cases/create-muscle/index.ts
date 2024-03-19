2989cfca2e4ce0412a4bdee202be46f0ac1d1ce7
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 08:28:27 -0300

    feat: add create-muscle use case

---
+++ src/modules/muscle/use-cases/create-muscle/index.ts
@@ -0,0 +1,48 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import MuscleDomain from '~/modules/muscle/domain/muscle.domain';
+import { CreateMuscleDto } from '~/modules/muscle/dto/create-muscle.dto';
+import { MuscleDto } from '~/modules/muscle/dto/muscle.dto';
+import MuscleMapper from '~/modules/muscle/mappers/muscle.mapper';
+import MuscleRepository from '~/services/database/typeorm/repositories/muscle-repository';
+
+export type CreateMuscleParams = CreateMuscleDto;
+export type CreateMuscleResult = MuscleDto;
+
+@Injectable()
+export class CreateMuscleUseCase {
+  constructor(
+    private muscleRepository: MuscleRepository,
+    private readonly muscleMapper: MuscleMapper,
+  ) {}
+
+  async execute({ name }: CreateMuscleParams): Promise<CreateMuscleResult> {
+    const muscle = MuscleDomain.create({ name });
+    if (muscle.isLeft()) {
+      throw new HttpException(
+        { message: muscle.value.message },
+        muscle.value.code,
+      );
+    }
+
+    const muscleCreatedOrError = await this.muscleRepository.create(
+      this.muscleMapper.toPersistence(muscle.value),
+    );
+
+    if (muscleCreatedOrError.isLeft()) {
+      throw new HttpException(
+        { message: muscleCreatedOrError.value.message },
+        muscleCreatedOrError.value.code,
+      );
+    }
+
+    const muscleDto = muscleCreatedOrError.value.toDto();
+    if (muscleDto.isLeft()) {
+      throw new HttpException(
+        { message: muscleDto.value.message },
+        muscleDto.value.code,
+      );
+    }
+
+    return muscleDto.value;
+  }
+}
---
+++ src/modules/muscle/use-cases/provider/index.ts
@@ -0,0 +1,5 @@
+import { CreateMuscleUseCase } from '~/modules/muscle/use-cases/create-muscle';
+
+const useCaseProviders = [CreateMuscleUseCase];
+
+export default useCaseProviders;
