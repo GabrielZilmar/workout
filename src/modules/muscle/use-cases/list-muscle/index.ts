9195bdd55a8a4a72b41e7f7f5895a9789e1095a5
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 15:29:57 -0300

    feat: add ListMuscle use case

---
+++ src/modules/muscle/use-cases/list-muscle/index.ts
@@ -0,0 +1,47 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { ListMusclesDto } from '~/modules/muscle/dto/list-muscles.dto';
+import { MuscleDto } from '~/modules/muscle/dto/muscle.dto';
+import MuscleRepository from '~/services/database/typeorm/repositories/muscle-repository';
+import { UseCase } from '~/shared/core/use-case';
+
+export type ListMuscleParams = ListMusclesDto;
+export type ListMuscleResult = {
+  items: MuscleDto[];
+  count: number;
+};
+
+@Injectable()
+export class ListMuscle implements UseCase<ListMuscleParams, ListMuscleResult> {
+  constructor(private readonly muscleRepository: MuscleRepository) {}
+
+  public async execute({
+    name,
+    skip,
+    take,
+  }: ListMuscleParams): Promise<ListMuscleResult> {
+    const { items, count } = await this.muscleRepository.find({
+      where: { name },
+      skip,
+      take,
+    });
+
+    const musclesDto: MuscleDto[] = [];
+    items.forEach((muscle) => {
+      const muscleDto = muscle.toDto();
+
+      if (muscleDto.isLeft()) {
+        throw new HttpException(
+          { message: muscleDto.value.message },
+          muscleDto.value.code,
+        );
+      }
+
+      musclesDto.push(muscleDto.value);
+    });
+
+    return {
+      items: musclesDto,
+      count,
+    };
+  }
+}
--- src/modules/muscle/use-cases/provider/index.ts
+++ src/modules/muscle/use-cases/provider/index.ts
@@ -1,5 +1,6 @@
 import { CreateMuscle } from '~/modules/muscle/use-cases/create-muscle';
+import { ListMuscle } from '~/modules/muscle/use-cases/list-muscle';
 
-const useCaseProviders = [CreateMuscle];
+const useCaseProviders = [CreateMuscle, ListMuscle];
 
 export default useCaseProviders;
