d162a2ddf7c0f2e1bd69a3b4b99b30c48cccd3ad
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 23:52:18 -0300

    fix: invlaid muscle name error message

--- src/modules/muscle/domain/errors.ts
+++ src/modules/muscle/domain/errors.ts
@@ -4,7 +4,7 @@ import { MIN_MUSCLE_NAME_LENGTH } from '~/modules/muscle/domain/value-objects/na
 export class MuscleDomainError extends Error {
   public readonly code: number;
   public static messages = {
-    invalidMuscleName: `Invalid Username, must be at least ${MIN_MUSCLE_NAME_LENGTH} characters`,
+    invalidMuscleName: `Invalid muscle name, must be at least ${MIN_MUSCLE_NAME_LENGTH} characters`,
     missingProps: 'Missing the muscle name to create it',
   };
 
