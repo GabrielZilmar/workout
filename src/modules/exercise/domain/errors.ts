77ee97c2f88a7dbdaff697fcd6da2d00056e17a5
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-20 15:26:10 -0300

    feat: add missing props error message

--- src/modules/exercise/domain/errors.ts
+++ src/modules/exercise/domain/errors.ts
@@ -4,6 +4,8 @@ import { MIN_EXERCISE_NAME_LENGTH } from '~/modules/exercise/domain/value-object
 export class ExerciseDomainError extends Error {
   public readonly code: number;
   public static messages = {
+    missingProps:
+      'Missing props to create a new exercise. Required: name and muscleId',
     invalidExerciseName: `Invalid exercise name, must be at least ${MIN_EXERCISE_NAME_LENGTH} characters`,
     invalidExerciseInfo: 'Exercise info cannot be an empty string',
     invalidTutorialUrl: 'Exercise tutorial URL need to be a valid URL',
