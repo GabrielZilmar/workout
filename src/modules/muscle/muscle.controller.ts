08b0cdf3767b84e7858ea067b653f8591a8d7e62
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-19 22:19:22 -0300

    feat: delete muscle route

--- src/modules/muscle/muscle.controller.ts
+++ src/modules/muscle/muscle.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
@@ -11,12 +12,14 @@ import {
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateMuscleDto } from '~/modules/muscle/dto/create-muscle.dto';
+import { DeleteMuscleParamsDto } from '~/modules/muscle/dto/delete-muscle.dto';
 import { ListMusclesDto } from '~/modules/muscle/dto/list-muscles.dto';
 import {
   UpdateMuscleBodyDto,
   UpdateMuscleParamsDto,
 } from '~/modules/muscle/dto/update-muscle.dto';
 import { CreateMuscle } from '~/modules/muscle/use-cases/create-muscle';
+import { DeleteMuscle } from '~/modules/muscle/use-cases/delete-muscle';
 import { ListMuscle } from '~/modules/muscle/use-cases/list-muscle';
 import { UpdateMuscle } from '~/modules/muscle/use-cases/update-muscle';
 
@@ -27,6 +30,7 @@ export class MuscleController {
     private readonly createMuscle: CreateMuscle,
     private readonly listMuscle: ListMuscle,
     private readonly updateMuscle: UpdateMuscle,
+    private readonly deleteMuscle: DeleteMuscle,
   ) {}
 
   @Post()
@@ -47,5 +51,11 @@ export class MuscleController {
     @Param() { id }: UpdateMuscleParamsDto,
   ) {
     return this.updateMuscle.execute({ id, ...body });
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminGuard)
+  delete(@Param() { id }: DeleteMuscleParamsDto) {
+    return this.deleteMuscle.execute({ id });
   }
 }
