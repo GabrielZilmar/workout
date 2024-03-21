5fe5f58e972d071c96ad67ae77b61d28c74ecc75
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 11:13:31 -0300

    feat: add toSimpleDto to exercise domain

--- src/modules/exercise/domain/exercise.domain.ts
+++ src/modules/exercise/domain/exercise.domain.ts
@@ -4,6 +4,7 @@ import ExerciseInfo from '~/modules/exercise/domain/value-objects/info';
 import ExerciseName from '~/modules/exercise/domain/value-objects/name';
 import ExerciseTutorialUrl from '~/modules/exercise/domain/value-objects/tutorial-url';
 import { ExerciseDto } from '~/modules/exercise/dto/exercise.dto';
+import { SimpleExerciseDto } from '~/modules/exercise/dto/simple-exercise.dto';
 import { AggregateRoot } from '~/shared/domain/aggregate-root';
 import { UniqueEntityID } from '~/shared/domain/unique-entity-id';
 import { Either, left, right } from '~/shared/either';
@@ -43,6 +44,10 @@ export default class ExerciseDomain extends AggregateRoot<ExerciseDomainProps> {
 
   public toDto() {
     return ExerciseDto.domainToDto(this);
+  }
+
+  public toSimpleDto() {
+    return SimpleExerciseDto.domainToDto(this);
   }
 
   public update({
