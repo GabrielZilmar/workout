b6bdfd68541564c157de3a69550543fc5bef7444
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 13:31:19 -0300

    feat: delete exercise use case

--- src/modules/exercise/use-cases/providers/index.ts
+++ src/modules/exercise/use-cases/providers/index.ts
@@ -1,4 +1,5 @@
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
+import { DeleteExercise } from '~/modules/exercise/use-cases/delete-exercise';
 import { GetExercise } from '~/modules/exercise/use-cases/get-exercise';
 import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
 import { UpdateExercise } from '~/modules/exercise/use-cases/update-exercise';
@@ -8,6 +9,7 @@ const useCaseProviders = [
   ListExercises,
   GetExercise,
   UpdateExercise,
+  DeleteExercise,
 ];
 
 export default useCaseProviders;
