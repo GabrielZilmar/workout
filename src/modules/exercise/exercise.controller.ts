c8da47625302e9082b8cf441c3f364025e90aeb5
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 12:59:40 -0300

    feat: add update to exercise controller

--- src/modules/exercise/exercise.controller.ts
+++ src/modules/exercise/exercise.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   Param,
   Post,
+  Put,
   Query,
   UseGuards,
 } from '@nestjs/common';
@@ -11,9 +12,14 @@ import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateExerciseDto } from '~/modules/exercise/dto/create-exercise.dto';
 import { ListExercisesDto } from '~/modules/exercise/dto/list-exercises.dto';
+import {
+  UpdateExerciseBodyDto,
+  UpdateExerciseParamsDto,
+} from '~/modules/exercise/dto/update-exercise.dto';
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
 import { GetExercise } from '~/modules/exercise/use-cases/get-exercise';
 import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
+import { UpdateExercise } from '~/modules/exercise/use-cases/update-exercise';
 
 @Controller('/api/exercises')
 @UseGuards(AuthGuard)
@@ -22,6 +28,7 @@ export class ExerciseController {
     private readonly createExercise: CreateExercise,
     private readonly listExercises: ListExercises,
     private readonly getExercise: GetExercise,
+    private readonly updateExercise: UpdateExercise,
   ) {}
 
   @Post()
@@ -38,5 +45,14 @@ export class ExerciseController {
   @Get(':idOrUsername')
   findOne(@Param('idOrUsername') idOrUsername: string) {
     return this.getExercise.execute({ idOrUsername });
+  }
+
+  @Put(':id')
+  @UseGuards(AdminGuard)
+  update(
+    @Param() { id }: UpdateExerciseParamsDto,
+    @Body() body: UpdateExerciseBodyDto,
+  ) {
+    return this.updateExercise.execute({ id, ...body });
   }
 }
