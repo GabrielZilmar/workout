3470c01a439825ee88fc48c8a33ac038dd12c3fd
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 12:59:15 -0300

    feat: add UpdateExercise use case

--- src/modules/exercise/use-cases/providers/index.ts
+++ src/modules/exercise/use-cases/providers/index.ts
@@ -1,7 +1,13 @@
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
 import { GetExercise } from '~/modules/exercise/use-cases/get-exercise';
 import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
+import { UpdateExercise } from '~/modules/exercise/use-cases/update-exercise';
 
-const useCaseProviders = [CreateExercise, ListExercises, GetExercise];
+const useCaseProviders = [
+  CreateExercise,
+  ListExercises,
+  GetExercise,
+  UpdateExercise,
+];
 
 export default useCaseProviders;
