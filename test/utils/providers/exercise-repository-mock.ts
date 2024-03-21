90d9b99ef2bbd52eff75055a6b09001ee80e7dc9
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 12:27:15 -0300

    feat: add findOne to exercise repository provider mock

--- test/utils/providers/exercise-repository-mock.ts
+++ test/utils/providers/exercise-repository-mock.ts
@@ -38,6 +38,10 @@ const getExerciseRepositoryProvider = ({
           .fn()
           .mockResolvedValue(exerciseDomain);
 
+        exerciseRepository.findOne = jest
+          .fn()
+          .mockResolvedValue(exerciseDomain);
+
         exerciseRepository.update = jest.fn().mockResolvedValue(right(true));
 
         exerciseRepository.delete = jest.fn().mockResolvedValue(right(true));
