6fbc28d87da055bc1564182cc4d678f07258a06c
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 13:31:47 -0300

    feat: add delete to exercise controller

--- src/modules/exercise/exercise.controller.ts
+++ src/modules/exercise/exercise.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
@@ -11,12 +12,14 @@ import {
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateExerciseDto } from '~/modules/exercise/dto/create-exercise.dto';
+import { DeleteExerciseParamsDto } from '~/modules/exercise/dto/delete-exercise.dto';
 import { ListExercisesDto } from '~/modules/exercise/dto/list-exercises.dto';
 import {
   UpdateExerciseBodyDto,
   UpdateExerciseParamsDto,
 } from '~/modules/exercise/dto/update-exercise.dto';
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
+import { DeleteExercise } from '~/modules/exercise/use-cases/delete-exercise';
 import { GetExercise } from '~/modules/exercise/use-cases/get-exercise';
 import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
 import { UpdateExercise } from '~/modules/exercise/use-cases/update-exercise';
@@ -29,6 +32,7 @@ export class ExerciseController {
     private readonly listExercises: ListExercises,
     private readonly getExercise: GetExercise,
     private readonly updateExercise: UpdateExercise,
+    private readonly deleteExercise: DeleteExercise,
   ) {}
 
   @Post()
@@ -54,5 +58,11 @@ export class ExerciseController {
     @Body() body: UpdateExerciseBodyDto,
   ) {
     return this.updateExercise.execute({ id, ...body });
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminGuard)
+  delete(@Param() { id }: DeleteExerciseParamsDto) {
+    return this.deleteExercise.execute({ id });
   }
 }
