253d3d118e2ac03adf8d1fd35261e824b244b110
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 12:13:56 -0300

    feat: find onde exercise route

--- src/modules/exercise/exercise.controller.ts
+++ src/modules/exercise/exercise.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateExerciseDto } from '~/modules/exercise/dto/create-exercise.dto';
 import { ListExercisesDto } from '~/modules/exercise/dto/list-exercises.dto';
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
+import { GetExercise } from '~/modules/exercise/use-cases/get-exercise';
 import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
 
 @Controller('/api/exercises')
@@ -12,6 +21,7 @@ export class ExerciseController {
   constructor(
     private readonly createExercise: CreateExercise,
     private readonly listExercises: ListExercises,
+    private readonly getExercise: GetExercise,
   ) {}
 
   @Post()
@@ -23,5 +33,10 @@ export class ExerciseController {
   @Get()
   findAll(@Query() query: ListExercisesDto) {
     return this.listExercises.execute({ ...query });
+  }
+
+  @Get(':idOrUsername')
+  findOne(@Param('idOrUsername') idOrUsername: string) {
+    return this.getExercise.execute({ idOrUsername });
   }
 }
