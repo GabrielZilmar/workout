7997a70169ae2dafcf2d2bfec8a7e95db2e0fc31
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-21 11:14:35 -0300

    feat: add findAll to exercise controller

--- src/modules/exercise/exercise.controller.ts
+++ src/modules/exercise/exercise.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { AdminGuard } from '~/guards/admin-guard';
 import { AuthGuard } from '~/guards/auth.guard';
 import { CreateExerciseDto } from '~/modules/exercise/dto/create-exercise.dto';
+import { ListExercisesDto } from '~/modules/exercise/dto/list-exercises.dto';
 import { CreateExercise } from '~/modules/exercise/use-cases/create-exercise';
+import { ListExercises } from '~/modules/exercise/use-cases/list-exercises';
 
 @Controller('/api/exercises')
 @UseGuards(AuthGuard)
 export class ExerciseController {
-  constructor(private readonly createExercise: CreateExercise) {}
+  constructor(
+    private readonly createExercise: CreateExercise,
+    private readonly listExercises: ListExercises,
+  ) {}
 
   @Post()
   @UseGuards(AdminGuard)
   create(@Body() body: CreateExerciseDto) {
     return this.createExercise.execute({ ...body });
+  }
+
+  @Get()
+  findAll(@Query() query: ListExercisesDto) {
+    return this.listExercises.execute({ ...query });
   }
 }
