34b7b9ca6143176ab03d221a91737209aa3877e1
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-25 13:52:52 -0300

    feat: add create-workout-exercise use case

---
+++ src/modules/workout-exercise/use-cases/providers/index.ts
@@ -0,0 +1,5 @@
+import { CreateWorkoutExercise } from '~/modules/workout-exercise/use-cases/create-workout-exercise';
+
+const useCaseProviders = [CreateWorkoutExercise];
+
+export default useCaseProviders;
