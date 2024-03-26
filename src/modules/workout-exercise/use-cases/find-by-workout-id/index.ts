fc5bc54c2513dc6223067cd755b545ee2e70dd2b
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-25 22:32:24 -0300

    feat: find-by-workout-id use case

---
+++ src/modules/workout-exercise/use-cases/find-by-workout-id/index.ts
@@ -0,0 +1,55 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { FindByWorkoutIdDto } from '~/modules/workout-exercise/dto/find-by-workout-id.dto';
+import { WorkoutExerciseDto } from '~/modules/workout-exercise/dto/workout-exercise.dto';
+import WorkoutExerciseRepository from '~/services/database/typeorm/repositories/workout-exercise-repository';
+import { UseCase } from '~/shared/core/use-case';
+import { PaginatedDto } from '~/shared/dto/paginated';
+
+type FindByWorkoutIdParams = FindByWorkoutIdDto &
+  PaginatedDto & { userId: string };
+type FindByWorkoutIdResponse = Promise<{
+  item: WorkoutExerciseDto[];
+  count: number;
+}>;
+
+@Injectable()
+export class FindByWorkoutId
+  implements UseCase<FindByWorkoutIdParams, FindByWorkoutIdResponse>
+{
+  constructor(
+    private readonly workoutExerciseRepository: WorkoutExerciseRepository,
+  ) {}
+
+  async execute({
+    userId,
+    workoutId,
+    skip,
+    take,
+  }: FindByWorkoutIdParams): FindByWorkoutIdResponse {
+    const { items, count } =
+      await this.workoutExerciseRepository.findUsersWorkoutExercises({
+        where: { userId, workoutId },
+        skip,
+        take,
+      });
+
+    const workoutsExercisesDto: WorkoutExerciseDto[] = [];
+    items.forEach((workout) => {
+      const workoutsExerciseDto = workout.toDto();
+
+      if (workoutsExerciseDto.isLeft()) {
+        throw new HttpException(
+          { message: workoutsExerciseDto.value.message },
+          workoutsExerciseDto.value.code,
+        );
+      }
+
+      workoutsExercisesDto.push(workoutsExerciseDto.value);
+    });
+
+    return {
+      item: workoutsExercisesDto,
+      count,
+    };
+  }
+}
--- src/modules/workout-exercise/use-cases/providers/index.ts
+++ src/modules/workout-exercise/use-cases/providers/index.ts
@@ -1,5 +1,6 @@
 import { CreateWorkoutExercise } from '~/modules/workout-exercise/use-cases/create-workout-exercise';
+import { FindByWorkoutId } from '~/modules/workout-exercise/use-cases/find-by-workout-id';
 
-const useCaseProviders = [CreateWorkoutExercise];
+const useCaseProviders = [CreateWorkoutExercise, FindByWorkoutId];
 
 export default useCaseProviders;
