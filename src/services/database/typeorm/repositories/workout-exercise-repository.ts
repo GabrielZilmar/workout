82722229ea62641ac12725168eb9c50d903403b7
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-25 22:31:14 -0300

    feat: add findUsersWorkoutExercises method to workout exercises repository

--- src/services/database/typeorm/repositories/workout-exercise-repository.ts
+++ src/services/database/typeorm/repositories/workout-exercise-repository.ts
@@ -4,6 +4,23 @@ import { WorkoutExercise } from '~/modules/workout-exercise/entities/workout-exe
 import WorkoutExerciseMapper from '~/modules/workout-exercise/mappers/workout-exercise.mapper';
 import { BaseRepository } from '~/services/database/typeorm/repositories/base/base-repository';
 
+const DEFAULT_TAKE_ITEMS = 20;
+const DEFAULT_SKIP_ITEMS = 0;
+
+type UsersWorkoutExercisesWhereOption = {
+  userId: string;
+  workoutId: string;
+};
+type UsersWorkoutExercisesFindParams = {
+  where: UsersWorkoutExercisesWhereOption;
+  skip?: number;
+  take?: number;
+};
+type UsersWorkoutExerciseFindResult = {
+  items: WorkoutExerciseDomain[];
+  count: number;
+};
+
 @Injectable()
 export default class WorkoutExerciseRepository extends BaseRepository<
   WorkoutExercise,
@@ -13,5 +30,33 @@ export default class WorkoutExerciseRepository extends BaseRepository<
 
   constructor(workoutExerciseMapper: WorkoutExerciseMapper) {
     super(WorkoutExercise, workoutExerciseMapper);
+  }
+
+  async findUsersWorkoutExercises({
+    where: { userId, workoutId },
+    take = DEFAULT_TAKE_ITEMS,
+    skip = DEFAULT_SKIP_ITEMS,
+  }: UsersWorkoutExercisesFindParams): Promise<UsersWorkoutExerciseFindResult> {
+    const [items, count] = await this.repository
+      .createQueryBuilder()
+      .select('we')
+      .from(WorkoutExercise, 'we')
+      .leftJoin('we.workout', 'w')
+      .where('we.workoutId = :workoutId', { workoutId })
+      .andWhere('w.userId = :userId OR w.isPrivate = false', { userId })
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const itemsToDomain: WorkoutExerciseDomain[] = [];
+    for await (const item of items) {
+      const itemToDomain = this.mapper.toDomain(item);
+
+      if (itemToDomain.isRight()) {
+        itemsToDomain.push(itemToDomain.value);
+      }
+    }
+
+    return { items: itemsToDomain, count };
   }
 }
