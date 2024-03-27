49cb669e1d5fce4e978d2a2dbb4ffa85ae486747
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-27 15:35:35 -0300

    feat: add update-set use case

--- src/modules/set/domain/use-cases/provider/index.ts
+++ src/modules/set/domain/use-cases/provider/index.ts
@@ -1,6 +1,7 @@
 import { CreateSet } from '~/modules/set/domain/use-cases/create-set';
 import { ListSetByWorkoutExerciseId } from '~/modules/set/domain/use-cases/list-set-by-workout-exercise';
+import { UpdateSet } from '~/modules/set/domain/use-cases/update-set';
 
-const setUseCaseProviders = [CreateSet, ListSetByWorkoutExerciseId];
+const setUseCaseProviders = [CreateSet, ListSetByWorkoutExerciseId, UpdateSet];
 
 export default setUseCaseProviders;
