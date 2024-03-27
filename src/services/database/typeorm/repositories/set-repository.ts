584adea48ac87c1f6878f03dac2cb378f3436266
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-27 11:50:42 -0300

    feat: add findByWorkoutExerciseId method to set-repository

--- src/services/database/typeorm/repositories/set-repository.ts
+++ src/services/database/typeorm/repositories/set-repository.ts
@@ -1,12 +1,61 @@
+import { Injectable } from '@nestjs/common';
 import SetDomain from '~/modules/set/domain/set.domain';
 import { Set } from '~/modules/set/entities/set.entity';
 import SetMapper from '~/modules/set/mappers/set.mapper';
 import { BaseRepository } from '~/services/database/typeorm/repositories/base/base-repository';
 
+const DEFAULT_TAKE_ITEMS = 10;
+const DEFAULT_SKIP_ITEMS = 0;
+
+type FindByWorkoutExerciseWhereOptions = {
+  userId: string;
+  workoutExerciseId: string;
+};
+type FindByWorkoutExerciseIdParams = {
+  where: FindByWorkoutExerciseWhereOptions;
+  skip?: number;
+  take?: number;
+};
+type FindByWorkoutExerciseIdResult = Promise<{
+  items: SetDomain[];
+  count: number;
+}>;
+
+@Injectable()
 export default class SetRepository extends BaseRepository<Set, SetDomain> {
   mapper: SetMapper;
 
   constructor(setMapper: SetMapper) {
     super(Set, setMapper);
+  }
+
+  async findByWorkoutExerciseId({
+    where: { userId, workoutExerciseId },
+    skip = DEFAULT_SKIP_ITEMS,
+    take = DEFAULT_TAKE_ITEMS,
+  }: FindByWorkoutExerciseIdParams): FindByWorkoutExerciseIdResult {
+    const [items, count] = await this.repository
+      .createQueryBuilder()
+      .select('set')
+      .from(Set, 'set')
+      .leftJoin('set.workoutExercise', 'we')
+      .where('we.id = :workoutExerciseId', {
+        workoutExerciseId,
+      })
+      .andWhere('(w.userId = :userId OR w.isPrivate = false)', { userId })
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const itemsToDomain: SetDomain[] = [];
+    for await (const item of items) {
+      const itemToDomain = this.mapper.toDomain(item);
+
+      if (itemToDomain.isRight()) {
+        itemsToDomain.push(itemToDomain.value);
+      }
+    }
+
+    return { items: itemsToDomain, count };
   }
 }
