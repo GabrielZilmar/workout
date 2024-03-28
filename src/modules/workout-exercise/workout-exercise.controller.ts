12cf1adcee293b62879ec66b5f4f627d8bfbf0ef
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-28 09:57:17 -0300

    feat: add get workout exercise details to controller

--- src/modules/workout-exercise/workout-exercise.controller.ts
+++ src/modules/workout-exercise/workout-exercise.controller.ts
@@ -14,6 +14,7 @@ import { AuthGuard } from '~/guards/auth.guard';
 import { CreateWorkoutExerciseDto } from '~/modules/workout-exercise/dto/create-workout-exercise.dto';
 import { DeleteWorkoutExerciseDto } from '~/modules/workout-exercise/dto/delete-workout-exercise.dto';
 import { FindByWorkoutIdDto } from '~/modules/workout-exercise/dto/find-by-workout-id.dto';
+import { GetWorkoutExerciseDetailsParamsDto } from '~/modules/workout-exercise/dto/get-workout-exercise-details.dto';
 import {
   UpdateWorkoutExerciseDto,
   UpdateWorkoutExerciseParamsDto,
@@ -21,6 +22,7 @@ import {
 import { CreateWorkoutExercise } from '~/modules/workout-exercise/use-cases/create-workout-exercise';
 import DeleteWorkoutExercise from '~/modules/workout-exercise/use-cases/delete-workout-exercise';
 import { FindByWorkoutId } from '~/modules/workout-exercise/use-cases/find-by-workout-id';
+import { GetWorkoutExerciseDetails } from '~/modules/workout-exercise/use-cases/get-workout-details';
 import { UpdateWorkoutExercise } from '~/modules/workout-exercise/use-cases/update-workout-exercise';
 import { PaginatedDto } from '~/shared/dto/paginated';
 import { RequestWithUser } from '~/shared/types/request';
@@ -33,6 +35,7 @@ export class WorkoutExerciseController {
     private readonly findByWorkoutIdUseCase: FindByWorkoutId,
     private readonly updateWorkoutExercise: UpdateWorkoutExercise,
     private readonly deleteWorkoutExercise: DeleteWorkoutExercise,
+    private readonly getWorkoutExerciseDetails: GetWorkoutExerciseDetails,
   ) {}
 
   @Post()
@@ -53,6 +56,18 @@ export class WorkoutExerciseController {
       workoutId,
       skip,
       take,
+    });
+  }
+
+  @Get('/details/:id')
+  findDetails(
+    @Req() req: RequestWithUser,
+    @Param() { id }: GetWorkoutExerciseDetailsParamsDto,
+  ) {
+    const userId = req.user.id;
+    return this.getWorkoutExerciseDetails.execute({
+      userId,
+      id,
     });
   }
 
