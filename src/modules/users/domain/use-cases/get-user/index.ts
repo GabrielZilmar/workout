3c477f99278150e3f23dba37ec33a1a4a5785255
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-03-27 21:07:10 -0300

    feat: only return user complete dto if user is requesting itself

--- src/modules/users/domain/use-cases/get-user/index.ts
+++ src/modules/users/domain/use-cases/get-user/index.ts
@@ -8,18 +8,20 @@ import { validate as validateUUID } from 'uuid';
 import { UserUseCaseError } from '~/modules/users/domain/use-cases/errors';
 import { UserDomain } from '~/modules/users/domain/users.domain';
 import { GetUserDto } from '~/modules/users/dto/get-user.dto';
+import { SimpleUserDto } from '~/modules/users/dto/simple-user.dto';
 import { UserDto } from '~/modules/users/dto/user.dto';
 import UserRepository from '~/services/database/typeorm/repositories/users-repository';
 import { UseCase } from '~/shared/core/use-case';
 
-type GetUserParams = GetUserDto;
-type GetUserResult = Promise<UserDto>;
+type GetUserParams = GetUserDto & { userId: string };
+type GetUserResult = Promise<UserDto | SimpleUserDto>;
 
 @Injectable()
 export class GetUser implements UseCase<GetUserParams, GetUserResult> {
   constructor(private readonly userRepository: UserRepository) {}
 
   public async execute({
+    userId,
     idOrUsername,
   }: GetUserParams): Promise<GetUserResult> {
     let user: UserDomain | null = null;
@@ -46,7 +48,8 @@ export class GetUser implements UseCase<GetUserParams, GetUserResult> {
       );
     }
 
-    const userDto = user.toDto();
+    const userDto =
+      user.id?.toValue() === userId ? user.toDto() : user.toSimpleDto();
     if (userDto.isLeft()) {
       throw new HttpException(userDto.value.message, userDto.value.code);
     }
