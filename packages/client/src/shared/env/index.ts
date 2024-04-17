a096174b8b61587c773b123f7fde33247d68a211
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-17 12:55:21 -0300

    feat: add env class

---
+++ packages/client/src/shared/env/index.ts
@@ -0,0 +1,23 @@
+export default class Env {
+  static get apiBaseUrl(): string {
+    const apiBaseUrl = this.getEnvOrThrow("VITE_API_BASE_URL");
+
+    return apiBaseUrl;
+  }
+
+  private static getEnvOrThrow(envName: string): string {
+    const env = import.meta.env[envName];
+    if (!env) {
+      throw new Error(`Missing environment variable ${envName}`);
+    }
+
+    return env;
+  }
+
+  private static getEnvOrDefault(
+    envName: string,
+    defaultValue: string
+  ): string {
+    return import.meta.env[envName] ?? defaultValue;
+  }
+}
