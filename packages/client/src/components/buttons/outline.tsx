839801efcf0916fc2bb470a43160e7839bc97391
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-20 15:58:32 -0300

    feat: add custom classNames to custom components

--- packages/client/src/components/buttons/outline.tsx
+++ packages/client/src/components/buttons/outline.tsx
@@ -1,15 +1,16 @@
 import { ButtonHTMLAttributes } from "react";
 
-export const StyledOutlineButton = (
-  props: ButtonHTMLAttributes<HTMLButtonElement>
-) => (
+export const StyledOutlineButton = ({
+  className,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
     {...props}
-    className="flex w-full justify-center
+    className={`
      rounded-md bg-indigo-600 px-3 py-1.5
      text-sm font-semibold leading-6 text-white shadow-sm
      hover:bg-indigo-500
-     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className}`}
   />
 );
 
--- packages/client/src/components/input/outline.tsx
+++ packages/client/src/components/input/outline.tsx
@@ -1,15 +1,16 @@
 import { InputHTMLAttributes } from "react";
 
-export const StyledOutlineInput = (
-  props: InputHTMLAttributes<HTMLInputElement>
-) => (
+export const StyledOutlineInput = ({
+  className,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement>) => (
   <input
     {...props}
-    className="block w-full
+    className={`w-full
     rounded-md border-0 py-1.5 pl-4
     text-gray-900 placeholder:text-gray-400
     focus:outline focus:ring-2 focus:ring-inset focus:ring-indigo-600
-    sm:text-sm sm:leading-6"
+    sm:text-sm sm:leading-6 ${className}`}
   />
 );
 
