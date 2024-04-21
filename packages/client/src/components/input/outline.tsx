2a26910f4d1823dcc5e80c1dbc21b0d2755d7a08
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-21 13:15:24 -0300

    feat: add register and erros form react hook form to custom outline input

--- packages/client/src/components/input/outline.tsx
+++ packages/client/src/components/input/outline.tsx
@@ -1,17 +1,45 @@
+import {
+  UseFormRegister,
+  FieldValues,
+  Path,
+  RegisterOptions,
+  FieldPath,
+  FieldError,
+} from "react-hook-form";
 import { InputHTMLAttributes } from "react";
 
-export const StyledOutlineInput = ({
+type StyledOutlineInputProps<T extends FieldValues> =
+  InputHTMLAttributes<HTMLInputElement> & {
+    register?: UseFormRegister<T>;
+    registerOptions?: RegisterOptions<T, FieldPath<T>>;
+    errors?: FieldError;
+    name: Path<T>;
+  };
+
+export const StyledOutlineInput = <T extends FieldValues>({
+  register,
+  registerOptions,
+  errors,
+  name,
   className,
   ...props
-}: InputHTMLAttributes<HTMLInputElement>) => (
-  <input
-    {...props}
-    className={`w-full
+}: StyledOutlineInputProps<T>) => (
+  <>
+    <input
+      {...props}
+      {...(name && register && register(name, registerOptions))}
+      className={`w-full
     rounded-md border-0 py-1.5 pl-4
     text-gray-900 placeholder:text-gray-400
     focus:outline focus:ring-2 focus:ring-inset focus:ring-indigo-600
     sm:text-sm sm:leading-6 ${className}`}
-  />
+    />
+    {errors && (
+      <span className="text-red-500" role="alert">
+        {errors.message}
+      </span>
+    )}
+  </>
 );
 
 export default StyledOutlineInput;
