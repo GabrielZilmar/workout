7b4eab1f1d2d1cdf4e71df7b77b22f44620c44b0
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-07-31 16:37:27 -0300

    feat: add forwardRef to ui components

--- packages/client/src/components/buttons/outline.tsx
+++ packages/client/src/components/buttons/outline.tsx
@@ -1,16 +1,22 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
-export const StyledOutlineButton = (
-  props: ButtonHTMLAttributes<HTMLButtonElement>
-) => (
-  <button
-    {...props}
-    className="flex w-full justify-center
-     rounded-md bg-indigo-600 px-3 py-1.5
-     text-sm font-semibold leading-6 text-white shadow-sm
-     hover:bg-indigo-500
-     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-  />
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const StyledOutlineButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ ...props }, ref) => {
+    return (
+      <button
+        className="flex w-full justify-center
+       rounded-md bg-indigo-600 px-3 py-1.5
+       text-sm font-semibold leading-6 text-white shadow-sm
+       hover:bg-indigo-500
+       focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        ref={ref}
+        {...props}
+      />
+    );
+  }
 );
+StyledOutlineButton.displayName = "StyledOutlineButton";
 
 export default StyledOutlineButton;
