9e8ed7818d863bb08d1cf33c1e5eb42786e548cf
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-01 18:06:09 -0300

    feat: add start and end icon to input component

--- packages/ui/lib/input/index.tsx
+++ packages/ui/lib/input/index.tsx
@@ -1,22 +1,38 @@
 import * as React from "react";
-
 import { cn } from "../utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:border-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="w-full relative">
+        {startIcon && (
+          <div className="absolute left-1.5 top-1/2 transform -translate-y-1/2">
+            {startIcon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:border-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            { "pl-10": !!startIcon },
+            { "pr-10": !!endIcon },
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {endIcon && (
+          <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+            {endIcon}
+          </div>
+        )}
+      </div>
     );
   }
 );
