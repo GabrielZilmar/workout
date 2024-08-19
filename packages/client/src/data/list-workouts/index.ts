eacf71136f294bb9be5a9b494644b138cb790404
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-19 11:02:30 -0300

    feat:add PaginationQuery type

--- packages/client/src/data/list-workouts/index.ts
+++ packages/client/src/data/list-workouts/index.ts
@@ -1,10 +1,11 @@
 import { AxiosError, AxiosResponse } from "axios";
 import axiosInstance from "~/data/api";
 import { GenericResponseError } from "~/types/api/errors";
+import { PaginationQuery } from "~/types/api/pagination-query";
 import { GenericListResponse } from "~/types/generic-list-response";
 import { Workout } from "~/types/workout";
 
-export type ListWorkoutsQuery = {
+export type ListWorkoutsQuery = PaginationQuery & {
   isRoutine?: boolean;
   name?: string;
 };
---
+++ packages/client/src/types/api/pagination-query.ts
@@ -0,0 +1,4 @@
+export type PaginationQuery = {
+  skip: number;
+  take: number;
+};
