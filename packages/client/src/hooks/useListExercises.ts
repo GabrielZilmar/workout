a356fa58fcce534776e8e8964ce4e35e654462cc
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-27 18:26:24 -0300

    feat: add useListPaginatedExercises

--- packages/client/src/hooks/useListExercises.ts
+++ packages/client/src/hooks/useListExercises.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import {
   listExercises,
   ListExercisesError,
@@ -6,6 +6,9 @@ import {
   ListExercisesResponse,
   ListExercisesResult,
 } from "~/data/list-exercises";
+
+const INITIAL_PAGE_PARAM = 0;
+const TAKE_20_EXERCISES = 20;
 
 const initialData: ListExercisesResult = {
   items: [],
@@ -23,4 +26,42 @@ export const useListExercises = (params: ListExercisesQuery = {}) => {
   });
 
   return { isLoading, isError, error, data: data?.data || initialData };
+};
+
+export const useListPaginatedExercises = (query: ListExercisesQuery = {}) => {
+  const {
+    isPending,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    data,
+  } = useInfiniteQuery({
+    queryKey: ["list-infinite-exercises", query],
+    queryFn: ({ pageParam = 0 }) =>
+      listExercises({ ...query, skip: pageParam, take: TAKE_20_EXERCISES }),
+    initialPageParam: INITIAL_PAGE_PARAM,
+    getNextPageParam: (lastPage, pages) => {
+      const hasNextPage =
+        lastPage.data.count >= pages.length * TAKE_20_EXERCISES;
+
+      return hasNextPage ? pages.length * TAKE_20_EXERCISES : undefined;
+    },
+    select: (data) => data.pages.flatMap((page) => page.data.items),
+  });
+
+  return {
+    isPending,
+    isLoading,
+    isSuccess,
+    isError,
+    error: error as ListExercisesError,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    data: data ?? [],
+  };
 };
