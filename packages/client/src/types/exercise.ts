1c0e92096669c9c912b2301f06d353415abce55e
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-27 18:02:51 -0300

    feat: add muscle type to client

--- packages/client/src/types/exercise.ts
+++ packages/client/src/types/exercise.ts
@@ -1,6 +1,10 @@
-export type SimpleExercise = {
+import { Muscle } from "~/types/muscle";
+
+export type Exercise = {
   id: string;
   name: string;
   muscleId: string;
   tutorialUrl: string | null;
+  info: string | null;
+  muscle?: Muscle;
 };
