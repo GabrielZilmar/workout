1d891c28453ecf6396f29fe6934536439c376145
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-28 09:50:10 -0300

    wip: workout details page

---
+++ packages/client/src/app/workout/[id]/page.tsx
@@ -0,0 +1,11 @@
+import GlobalLayout from "~/layouts/global.layout";
+
+const WorkoutDetailsPage = () => {
+  return (
+    <GlobalLayout>
+      <h1>Workout Details</h1>
+    </GlobalLayout>
+  );
+};
+
+export default WorkoutDetailsPage;
