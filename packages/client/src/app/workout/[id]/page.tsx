f68d8ff58ccaf1de494790ad6f077cf3710a2982
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-28 11:06:23 -0300

    wip: workout details page

--- packages/client/src/app/workout/[id]/page.tsx
+++ packages/client/src/app/workout/[id]/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import Error from "~/components/error";
+import Loading from "~/components/loading";
+import { useGetWorkout } from "~/hooks";
 import GlobalLayout from "~/layouts/global.layout";
 
 const WorkoutDetailsPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data, isLoading, isError, error } = useGetWorkout(id);
+
+  if (isError) {
+    const errorMessage = `${error?.response?.data?.message || ""}\n ${
+      error?.response?.statusText || ""
+    }`;
+    return <Error errorMessage={errorMessage} />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <GlobalLayout>
-      <h1>Workout Details</h1>
+      <div>
+        <h1 className="text-3xl font-bold">{data?.name || "-"}</h1>
+        <p className="text-xs">
+          {data?.isPrivate ? "Private Workout" : "Public Workout"}
+        </p>
+      </div>
     </GlobalLayout>
   );
 };
