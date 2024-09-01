f748ad5f440cfecbf2fd560e6f339be059ab1532
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-01 18:20:46 -0300

    wip: workout details page

--- packages/client/src/app/workout/[id]/page.tsx
+++ packages/client/src/app/workout/[id]/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@workout/ui";
 import { useParams } from "next/navigation";
 import Error from "~/components/error";
 import Loading from "~/components/loading";
-import { useGetWorkout } from "~/hooks";
+import { useGetWorkout, useGetWorkoutExercises } from "~/hooks";
 import GlobalLayout from "~/layouts/global.layout";
 
 const WorkoutDetailsPage = () => {
   const { id } = useParams<{ id: string }>();
-  const { data, isLoading, isError, error } = useGetWorkout(id);
+  const { data: workout, isLoading, isError, error } = useGetWorkout(id);
+  const { data: workoutExerciseData, isLoading: isLoadingWorkoutExercises } =
+    useGetWorkoutExercises({ workoutId: id });
 
   if (isError) {
     const errorMessage = `${error?.response?.data?.message || ""}\n ${
@@ -23,11 +31,32 @@ const WorkoutDetailsPage = () => {
 
   return (
     <GlobalLayout>
-      <div>
-        <h1 className="text-3xl font-bold">{data?.name || "-"}</h1>
+      <div className="py-4 px-6">
+        <h1 className="text-3xl font-bold">{workout?.name || "-"}</h1>
         <p className="text-xs">
-          {data?.isPrivate ? "Private Workout" : "Public Workout"}
+          {workout?.isPrivate ? "Private Workout" : "Public Workout"}
         </p>
+        <div className="py-8">
+          <h2 className="text-2xl font-bold">Exercises</h2>
+          {isLoadingWorkoutExercises ? (
+            <Loading />
+          ) : (
+            <div className="px-4">
+              {workoutExerciseData?.items.map((workoutExercise) => (
+                <Accordion key={workoutExercise.id} type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>
+                      {workoutExercise.exercise?.name}
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </GlobalLayout>
   );
