7c8c436b63424a0ebfc64c37a3e7d1bd9db2237d
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-03 16:39:41 -0300

    feat: remove node_env from dockerfile and add do workflow

--- packages/server/docker-entrypoint.sh
+++ packages/server/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-NODE_ENV=production npm run migration:up
-NODE_ENV=production npm run build
-NODE_ENV=production npm run start:prod
+npm run migration:up
+npm run build
+npm run start:prod
