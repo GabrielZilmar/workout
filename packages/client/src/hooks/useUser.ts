735f716b3a67897b1741c33aeaf88d7189976de1
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-09 12:32:58 -0300

    feat: add isLoading to useUser

--- packages/client/src/hooks/useUser.ts
+++ packages/client/src/hooks/useUser.ts
@@ -3,13 +3,16 @@ import { useQuery } from "@tanstack/react-query";
 import getUser, { GetUserResult } from "~/data/user/get-user";
 
 export const useUser = () => {
-  const { isPending, error, data } = useQuery<GetUserResult, AxiosError>({
+  const { isPending, isLoading, error, data } = useQuery<
+    GetUserResult,
+    AxiosError
+  >({
     queryKey: ["sessionUserData"],
     queryFn: () => getUser(),
     retry: 0,
   });
 
-  return { isPending, error, user: data?.data || null };
+  return { isPending, isLoading, error, user: data?.data || null };
 };
 
 export default useUser;
