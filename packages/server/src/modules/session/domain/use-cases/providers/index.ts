e5dd67e35287385609de06fcb6fec114db3d9ed1
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-09 17:17:28 -0300

    feat: add SendRecoverPassword use-case

--- packages/server/src/modules/session/domain/use-cases/providers/index.ts
+++ packages/server/src/modules/session/domain/use-cases/providers/index.ts
@@ -1,7 +1,13 @@
 import { Login } from '~/modules/session/domain/use-cases/login';
+import { SendRecoverPassword } from '~/modules/session/domain/use-cases/send-recover-password';
 import { SendVerifyEmail } from '~/modules/session/domain/use-cases/send-verify-email';
 import { VerifyEmail } from '~/modules/session/domain/use-cases/verify-email';
 
-const SessionUseCaseProviders = [Login, SendVerifyEmail, VerifyEmail];
+const SessionUseCaseProviders = [
+  Login,
+  SendVerifyEmail,
+  VerifyEmail,
+  SendRecoverPassword,
+];
 
 export default SessionUseCaseProviders;
