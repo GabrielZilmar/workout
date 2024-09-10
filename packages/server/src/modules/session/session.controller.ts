b1aa1019e972f33356204fd7e6bea2ad5cbe35a4
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-09 21:22:41 -0300

    feat: add send recover password email endpoint

--- packages/server/src/modules/session/session.controller.ts
+++ packages/server/src/modules/session/session.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { Login } from '~/modules/session/domain/use-cases/login';
+import { SendRecoverPassword } from '~/modules/session/domain/use-cases/send-recover-password';
 import { SendVerifyEmail } from '~/modules/session/domain/use-cases/send-verify-email';
 import { VerifyEmail } from '~/modules/session/domain/use-cases/verify-email';
 import { SessionLoginDto } from '~/modules/session/dto/login.dto';
+import { SendRecoverPasswordEmailBodyDTO } from '~/modules/session/dto/send-recover-password-email';
 import { SendVerifyEmailDto } from '~/modules/session/dto/send-verify-email.dto';
 import { VerifyEmailBodyDto } from '~/modules/session/dto/verify-email.dto';
 
@@ -12,6 +14,7 @@ export class SessionController {
     private readonly loginUseCase: Login,
     private readonly sendVerifyEmailUseCase: SendVerifyEmail,
     private readonly verifyEmailUseCase: VerifyEmail,
+    private readonly sendRecoverPassword: SendRecoverPassword,
   ) {}
 
   @Post('/login')
@@ -29,5 +32,12 @@ export class SessionController {
   @Post('/verify-email')
   verifyEmail(@Body() verifyEmailDto: VerifyEmailBodyDto) {
     return this.verifyEmailUseCase.execute(verifyEmailDto);
+  }
+
+  @Post('/send-recover-password-email')
+  sendRecoverPasswordEmail(@Body() body: SendRecoverPasswordEmailBodyDTO) {
+    return this.sendRecoverPassword.execute({
+      ...body,
+    });
   }
 }
