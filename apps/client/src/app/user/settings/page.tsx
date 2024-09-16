7107c05d0c635401bfe7d3a6b74b1d903ac385a8
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-16 16:21:45 -0300

    wip: user settings page

---
+++ apps/client/src/app/user/settings/page.tsx
@@ -0,0 +1,16 @@
+const UserSettingsPage = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <div>
+        <h2>Account</h2>
+        <div>
+          <h3>Email</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserSettingsPage;
