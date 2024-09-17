c18222e25a56e4a70f16d8d7001b00946537b452
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-16 21:56:14 -0300

    fix:regexPasswordValidation

--- apps/client/src/shared/validator.ts
+++ apps/client/src/shared/validator.ts
@@ -1,7 +1,7 @@
 export default class Validator {
   // Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
   public static regexPasswordValidation = new RegExp(
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9]).{8,}$/
   );
 
   public static isString(value: unknown): boolean {
