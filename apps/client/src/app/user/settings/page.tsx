a2324c17b26b027257146bdc4ee50196f6cc57b9
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-09-16 21:32:02 -0300

    feat: user settings page

--- apps/client/src/app/user/settings/page.tsx
+++ apps/client/src/app/user/settings/page.tsx
@@ -1,15 +1,51 @@
+"use client";
+
+import UserSettingForm from "~/components/forms/user/settings";
+import Loading from "~/components/loading";
+import { useUpdateUser, useUser } from "~/hooks";
+import GlobalLayout from "~/layouts/global.layout";
+
+type HandleSubmitParams = {
+  username?: string;
+  age?: number;
+  weight?: number;
+  height?: number;
+};
+
 const UserSettingsPage = () => {
+  const { user, isLoading } = useUser();
+  const { updateUserMutation } = useUpdateUser();
+
+  const handleSubmit = (data: HandleSubmitParams) => {
+    if (!user) return;
+
+    updateUserMutation({
+      ...data,
+      id: user.id,
+    });
+  };
+
+  if (!user && !isLoading) {
+    <h1 className="text-4xl text-red-600">User not found</h1>;
+  }
+
   return (
-    <div>
-      <h1>Settings</h1>
-      <div>
-        <h2>Account</h2>
-        <div>
-          <h3>Email</h3>
-          <p></p>
+    <GlobalLayout>
+      {!user ? (
+        <Loading />
+      ) : (
+        <div className="h-full">
+          <h1 className="text-4xl">Account</h1>
+          <div className="mt-2 px-2">
+            <UserSettingForm
+              user={user}
+              onSubmit={handleSubmit}
+              hideCancelLabel
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </GlobalLayout>
   );
 };
 
